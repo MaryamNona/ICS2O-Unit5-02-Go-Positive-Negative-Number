af82b535d2834de9aa3999803478a5e36564446c
MaryamNona <82899236+MaryamNona@users.noreply.github.com>
2021-06-10 00:35:01 +0000

    added code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ICS2O-Unit5-02-Go-Positive-Negative-Number
+
+[![GitHub's Super Linter](https://github.com/MaryamNona/ICS2O-Unit5-02-Go-Positive-Negative-Number/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/MaryamNona/ICS2O-Unit5-02-Go-Positive-Negative-Number/actions)
---
+++ negativePositiveNumber.go
@@ -0,0 +1,27 @@
+// Created by: Maryam Nona
+// Createdon: May 2021
+//
+// This program does basic math
+
+package main
+
+import (
+	"fmt"
+)
+
+func main () {
+	var userGuess int
+	
+	// input
+	fmt.Println("This program chooses a random number between 1-6 ")
+	fmt.Println()
+	fmt.Print("Enter number: ")
+	fmt.Scanln(&userGuess)
+
+	// output
+	if userGuess < 0 {
+		fmt.Println("You chose a negative number!")
+	} else {
+		fmt.Println("You chose a positive number!")
+	}
+}
