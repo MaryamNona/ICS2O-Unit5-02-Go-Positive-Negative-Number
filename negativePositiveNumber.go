83c4b4af6ce414bff1fce7a0eb05b14bc9b08d2d
MaryamNona <82899236+MaryamNona@users.noreply.github.com>
2021-06-10 01:04:04 +0000

    rewrote code

--- negativePositiveNumber.go
+++ negativePositiveNumber.go
@@ -10,18 +10,18 @@ import (
 )
 
 func main () {
-	var userGuess int
+	var userInteger int
 
 	// input
-	fmt.Println("This program chooses a random number between 1-6 ")
+	fmt.Println("This programs shows if user chose negative or positive number.")
 	fmt.Println()
-	fmt.Print("Enter number: ")
-	fmt.Scanln(&userGuess)
+	fmt.Println("Enter Number (-/+): ")
+	fmt.Scanln(&userInteger)
 
 	// output
-	if userGuess < 0 {
-		fmt.Println("You chose a negative number!")
-	} else {
-		fmt.Println("You chose a positive number!")
+	if userInteger < 0 {
+		fmt.Println("You chose a negative number.")
+  } else {
+		fmt.Println("You chose a positive number.")
 	}
 }
